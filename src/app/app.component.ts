f5b6d8dd6ecfbf1ae2a3dee4a7a80d9ee79d55ca
Francisco Viana <franciscosantos29@gmail.com>
2024-10-03 15:20:36 -0300

    Subindo aplicação em Angular de To Do

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TodoService } from './todo.service';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit {
+
+  todos: Todo[] = []
+
+  form: FormGroup = new FormGroup({
+    description: new FormControl('', [Validators.required, Validators.minLength(4)])
+  })
+
+  constructor(private service: TodoService) { }
+
+  ngOnInit(): void {
+    this.listarTodos()
+  }
+
+  listarTodos() {
+    this.service.listar().subscribe(todoList => {
+      console.log(todoList)
+      this.todos = todoList
+    });
+  }
+
+  submit() {
+    const todo: Todo = { ...this.form.value }
+    this.service.salvar(todo).subscribe(savedTodo => {
+      this.todos.push(savedTodo)
+      this.form.reset()
+    })
+  }
+
+  delete(todo: Todo) {
+    this.service.deletar(todo.id).subscribe({
+      next: (response) => this.listarTodos()
+    })
+  }
+
+  done(todo: Todo) {
+    this.service.marcarComoConcluido(todo.id).subscribe({
+      next: (todoAtualizado) => {
+        todo.done = todoAtualizado.done
+        todo.doneDate = todoAtualizado.doneDate
+      }
+    })
+  }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiURL: 'https://todo-api-francisco-1466273038da.herokuapp.com/api/todos'
+};
